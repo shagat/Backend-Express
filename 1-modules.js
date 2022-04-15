f7f169b41ef0541e84af8c9918847ba0cbb43614
shagat <shagat.bing@gmail.com>
2022-04-15 00:03:18 -0700

    first commit

---
+++ 1-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is a module by default
+// Modules - Encapsulated Code (Shares minimum)
+const names = require('./1-modules-2')
+const sayHi = require('./1-modules-3')
+const data = require('./1-modules-alt-way')
+require('./mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
