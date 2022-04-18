60491fa193765bd309fef07b8576597426099cea
shagat <shagat.bing@gmail.com>
2022-04-17 22:19:23 -0700

    Made a several files on async,promise and await as sl no. 6 and 7

---
+++ 7-using-promisify.js
@@ -0,0 +1,20 @@
+//
+//##### Alt way 2: Using promisify 
+const {readFile, writeFile} = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+    try {
+        const first = await readFilePromise('./content/first.txt','utf8')
+        const second = await readFilePromise('./content/second.txt','utf8')
+        await writeFilePromise('./content/using_promisify_output.txt',
+        `This is from 7-using promisify: ${first} ${second}`,
+        {flag: 'a'})
+        console.log(first,second)
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
